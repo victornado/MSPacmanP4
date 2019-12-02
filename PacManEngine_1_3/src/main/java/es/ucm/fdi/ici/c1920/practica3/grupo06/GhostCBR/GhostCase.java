c931c1526fb17b2d9b25d92be9dffd594799a0a8
victo <victo@DESKTOP-67GBJ6U>
2019-12-02 10:39:38 +0100

    de perdidos al rio con jcolibri

--- PacManEngine_1_3/src/main/java/es/ucm/fdi/ici/c1920/practica3/grupo06/GhostCBR/GhostCase.java
+++ PacManEngine_1_3/src/main/java/es/ucm/fdi/ici/c1920/practica3/grupo06/GhostCBR/GhostCase.java
@@ -1,9 +1,8 @@
 package es.ucm.fdi.ici.c1920.practica3.grupo06.GhostCBR;
 
-import es.ucm.fdi.gaia.jcolibri.cbrcore.Attribute;
-import es.ucm.fdi.gaia.jcolibri.cbrcore.CaseComponent;
+import ucm.gaia.jcolibri.cbrcore.Attribute;
 
-public class GhostCase implements CaseComponent{
+public class GhostCase implements ucm.gaia.jcolibri.cbrcore.CaseComponent{
 
 	@Override
 	public Attribute getIdAttribute() {
